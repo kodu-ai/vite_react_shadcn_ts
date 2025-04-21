a994ed4dbe79247c2d61bb99e4dcab2310c3f13e
matannahmani <matanmatan999new@gmail.com>
2025-04-21 20:14:01 +0900

    remove test route

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/test" element={<span> test</span>} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
