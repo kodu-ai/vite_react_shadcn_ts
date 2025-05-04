f22a64a9921381c4684a83b73c288896448459b7
matannahmani <matanmatan999new@gmail.com>
2025-05-05 04:02:37 +0900

    checkpoint: OuimmJR1pNT-aarjegylF

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,18 @@
 /// <reference types="vite/client" />
+
+interface ViteTypeOptions {
+  // By adding this line, you can make the type of ImportMetaEnv strict
+  // to disallow unknown keys.
+  strictImportMetaEnv: unknown;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_ID: string | undefined;
+  readonly VITE_SUPABASE_PUBLISHABLE_KEY: string | undefined;
+  readonly VITE_SUPABASE_SCHEMA: 'public' | 'staging' | undefined;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
