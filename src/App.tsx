b9241b29d5d3c8779830e49dbaacf29de3213518
matannahmani <matanmatan999new@gmail.com>
2025-05-06 20:35:15 +0900

    bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Toaster } from '@/components/ui/sonner'; // For notifications
 
 import LandingPage from './pages/LandingPage';
-import BookingPage from './pages/BookingPage';
-import DashboardPage from './pages/DashboardPage';
-import NotFoundPage from './pages/NotFound'; // Assuming NotFound exists
+
+import NotFoundPage from './pages/NotFound';
 import { Navbar } from './components/Navbar';
 import AppProvider from './components/app-provider';
 
@@ -17,8 +16,6 @@ function App() {
           <main className="flex-1">
             <Routes>
               <Route path="/" element={<LandingPage />} />
-              <Route path="/book" element={<BookingPage />} />
-              <Route path="/dashboard" element={<DashboardPage />} />
               <Route path="*" element={<NotFoundPage />} />
             </Routes>
           </main>
