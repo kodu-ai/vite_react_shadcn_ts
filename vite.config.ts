1ee6b6af93bcece34f3381439d30555c678677d0
matannahmani <matanmatan999new@gmail.com>
2025-05-08 15:19:21 +0900

    remove hmr overlay

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: '0.0.0.0',
     port: 5555,
+    hmr: {
+      overlay: false,
+    },
   },
   plugins: [react(), mode === 'development' && componentTagger()].filter(
     Boolean
